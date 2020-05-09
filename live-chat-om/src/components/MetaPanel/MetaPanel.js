436b7d3ef1eda5bc47a0c7bc0c1966e5592bb03b
ofeck <ofeckm3@gmail.com>
2020-05-09 21:20:43 +0300

    metaPanel
    added a metaPanel component

--- live-chat-om/src/components/MetaPanel/MetaPanel.js
+++ live-chat-om/src/components/MetaPanel/MetaPanel.js
@@ -1,11 +1,93 @@
 import React from 'react';
+import { Segment, Accordion, Header, Icon, Image, List } from 'semantic-ui-react';
 
 class MetaPanel extends React.Component {
-    render() {
+    state = {
+        channel: this.props.currentChannel,
+        isPrivateChannel: this.props.isPrivateChannel,
+        activeIndex: 0
+    };
+    
+    setActiveIndex = (event, titleProps) => {
+        const { index } = titleProps;
+        const { activeIndex } = this.state;
+        const newIndex = activeIndex === index ? -1 : index;
+        this.setState({ activeIndex: newIndex });
+    };
+
+    displayTopPosters = (posts) => {
         return (
-            <div>
-                MetaPanel
-            </div>
+            Object.entries(posts)
+            .sort((a, b) => b[1] - a[1])
+             .map(([key, val], i) => (
+               <List.Item key={i}>
+                   <Image avatar src={val.avatar} />
+                   <List.Content>
+                       <List.Header as="a">{key}</List.Header>
+                       <List.Description>{val.count} post{val.count > 1 ? 's' : ''}</List.Description>
+                   </List.Content>
+               </List.Item>
+             )).slice(0, 5)
+        )
+    };
+    
+    render() {
+        const { activeIndex, isPrivateChannel, channel } = this.state;
+        const { userPosts } = this.props;
+
+        if(isPrivateChannel) return null;
+
+        return (
+            <Segment loading={!channel}>
+                <Header as="h3" attached="top">
+                    About # {channel && channel.name}
+                </Header>
+                <Accordion styled attached="true">
+                    <Accordion.Title
+                        active={activeIndex === 0}
+                        index={0}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name="dropdown" />
+                        <Icon name="info" />
+                        Channel Details
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 0}>
+                        {channel && channel.details}
+                    </Accordion.Content>
+
+                    <Accordion.Title
+                        active={activeIndex === 1}
+                        index={1}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name="dropdown" />
+                        <Icon name="user circle" />
+                        Top Posters
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 1}>
+                        <List>
+                            {userPosts && this.displayTopPosters(userPosts)}    
+                        </List>
+                    </Accordion.Content>
+
+                    <Accordion.Title
+                        active={activeIndex === 2}
+                        index={2}
+                        onClick={this.setActiveIndex}
+                    >
+                        <Icon name="dropdown" />
+                        <Icon name="pencil alternate" />
+                        Created by
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 2}>
+                        <Header as="h3">
+                            <Image circular src={channel && channel.createdBy.avatar} />
+                            {channel && channel.createdBy.name}
+                        </Header>
+                    </Accordion.Content>
+                </Accordion>
+            </Segment>
         );
     };
 };
